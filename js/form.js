66c3f888cf367171ba5fc05cd443f4cdcc25629a
TanyaEdyna <t.yamkova@gmail.com>
2023-06-05 18:50:55 +0300

    js file

--- js/form.js
+++ js/form.js
@@ -1,1 +1,22 @@
+const goToServices = document.querySelector('#go-to-services');
+const goToLegislation = document.querySelector('#go-to-legislation');
+const goToContacts = document.querySelector('#go-to-contacts');
+const services = document.querySelector('.services');
+const legislation = document.querySelector('.legislation');
+const contacts = document.querySelector('.footer_title')
 
+goToServices.addEventListener('click', function (e) {
+    e.preventDefault();
+    services.scrollIntoView({ behavior: 'smooth' });
+
+});
+
+goToLegislation.addEventListener('click', function (e) {
+    e.preventDefault();
+    legislation.scrollIntoView({ behavior: 'smooth' });
+});
+
+goToContacts.addEventListener('click', function (e) {
+    e.preventDefault();
+    contacts.scrollIntoView({ behavior: 'smooth' });
+});
