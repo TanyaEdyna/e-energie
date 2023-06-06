09b51cbbd32b25d26b7b1dc9b245e5856c81f2a9
TanyaEdyna <t.yamkova@gmail.com>
2023-06-06 18:32:53 +0300

    js

--- js/form.js
+++ js/form.js
@@ -1,22 +1,16 @@
-const goToServices = document.querySelector('#go-to-services');
-const goToLegislation = document.querySelector('#go-to-legislation');
-const goToContacts = document.querySelector('#go-to-contacts');
-const services = document.querySelector('.services');
-const legislation = document.querySelector('.legislation');
-const contacts = document.querySelector('.footer_title')
 
-goToServices.addEventListener('click', function (e) {
-    e.preventDefault();
-    services.scrollIntoView({ behavior: 'smooth' });
+const scrollLinks = document.querySelectorAll('a[href*="#"]');
 
-});
+scrollLinks.forEach(function (link) {
+    link.addEventListener('click', function (event) {
+        event.preventDefault();
 
-goToLegislation.addEventListener('click', function (e) {
-    e.preventDefault();
-    legislation.scrollIntoView({ behavior: 'smooth' });
-});
+        const blockID = link.getAttribute('href').substring(1);
+        link = document.getElementById(blockID).scrollIntoView({
+            behavior: 'smooth',
+            block: 'center'
+        })
+    })
+})
 
-goToContacts.addEventListener('click', function (e) {
-    e.preventDefault();
-    contacts.scrollIntoView({ behavior: 'smooth' });
-});
+
