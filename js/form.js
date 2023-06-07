48b66e6cb835cdaadfd91b7fce2104c1bac32fe5
TanyaEdyna <t.yamkova@gmail.com>
2023-06-07 14:39:04 +0300

    Update form.js

--- js/form.js
+++ js/form.js
@@ -5,7 +5,7 @@ scrollLinks.forEach(function (link) {
     link.addEventListener('click', function (event) {
         event.preventDefault();
 
-        const blockID = link.getAttribute('href').substring(1);
+        const blockID = this.getAttribute('href').substring(1);
         link = document.getElementById(blockID).scrollIntoView({
             behavior: 'smooth',
             block: 'center'
